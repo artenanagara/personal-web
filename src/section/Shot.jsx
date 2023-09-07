961a5a0f0db47a031d58f7c5c9d5e8157ae262de
Artena <artenadhytyan22@gmail.com>
2023-09-07 15:01:10 +0700

    fix image

--- src/section/Shot.jsx
+++ src/section/Shot.jsx
@@ -11,14 +11,14 @@ function Shot() {
             <img src="/src/assets/img/shots/Shot 1.png" alt="" className='w-full' />
           </div>
           <div className='w-1/4 flex flex-col gap-5'>
-              <img src="/src/assets/img/shots/Shot 2.png" alt="" className='w-full h-full'/>
-              <img src="/src/assets/img/shots/Shot 3.png" alt="" className='w-full h-full'/>
+              <img src="../assets/img/shots/Shot 2.png" alt="" className='w-full h-full'/>
+              <img src="../assets/img/shots/Shot 3.png" alt="" className='w-full h-full'/>
           </div>
         </div>
         <div className='w-full flex flex-row gap-5 h-full'>
           <div className='w-1/4 flex flex-col gap-5'>
-              <img src="/src/assets/img/shots/Shot 5.png" alt="" className='w-full h-full'/>
-              <img src="/src/assets/img/shots/Shot 6.png" alt="" className='w-full h-full'/>
+              <img src="../assets/img/shots/Shot 5.png" alt="" className='w-full h-full'/>
+              <img src="../assets/img/shots/Shot 6.png" alt="" className='w-full h-full'/>
           </div>
           <div className='w-3/4'>
             <img src="/src/assets/img/shots/Shot 4.png" alt="" className='w-full' />
