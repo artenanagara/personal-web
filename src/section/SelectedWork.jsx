8283fdd243798e6f06f8287c17598879dcf30a6d
Artena <artenadhytyan22@gmail.com>
2023-12-20 22:47:38 +0700

    edited padding

--- src/section/SelectedWork.jsx
+++ src/section/SelectedWork.jsx
@@ -4,7 +4,7 @@ import CardPorto from "../components/CardPorto";
 function SelectedWork() {
   return (
     <>
-      <h1 className="text-4xl text-white font-bold pb-8">Selected Work</h1>
+      <h1 className="text-4xl text-white font-bold pb-4">Selected Work</h1>
       <CardPorto />
     </>
   );
